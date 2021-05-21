1e899ba8a560f7a65635218e35f5d360f54b175e
khanguyen01it <kha.nguyen01.it@gmail.com>
2021-05-21 16:50:34 +0700

    doing send mail

--- src/components/Account/signUp.jsx
+++ src/components/Account/signUp.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import emailjs from 'emailjs-com';
 import NavBar from '../Header/NavBar';
-
+// field send mail: to_name, to_email, code
 const SignUp = () => {
+    const [code, setCode] = useState(0);
+    const [to_name, setName] = useState();
+    const [to_email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [cfpassword, setCfpassword] = useState();
+    const [errorText, setErrorText] = useState('');
+    
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('default_service', 'template_yomost', e.target, `user_eGZkjyOWcdrxHJK1InigS`)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
+
+    const rand = () => {
+        const min = 111111;
+        const max = 999999;
+        const rand = min + Math.random() * (max - min);
+        setCode(Math.round(rand));
+        console.log(code);
+    }
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        // if()
+        console.log(to_name);
+        console.log(to_email);
+        console.log(password);
+        console.log(cfpassword);
+        console.log(code);
+        if(password != cfpassword) {
+            setErrorText('Your password not match! Try again.');
+        }
+        else {
+            
+        }
+    }
+
+    useEffect(() => {
+        rand();
+    }, []);
+
     return (
         <>
             <NavBar/>
@@ -9,37 +57,38 @@ const SignUp = () => {
                 <section className="section cart__area">
                     <div className="container">
                         <div className="responsive__cart-area">
-                        <div className="signin__form">
-                            <div className="header">
-                                <h2>Sign Up</h2>
-                                <p>Sign up with your information</p>
+                            <div className="signin__form">
+                                <div className="header">
+                                    <h2>Sign Up</h2>
+                                    <p>Sign up with your information</p>
+                                </div>
+                                <form className="wrapper" onSubmit={handleSignup}>
+                                    <div className="input-data">
+                                        <input type="text" name="to_name" onChange={e => setName(e.target.value)} required />
+                                        <div className="underline" />
+                                        <label>Fullname</label>
+                                    </div>
+                                    <div className="input-data">
+                                        <input type="email" name="to_email" onChange={e => setEmail(e.target.value)} required />
+                                        <div className="underline" />
+                                        <label>Email address</label>
+                                    </div>
+                                    <div className="input-data">
+                                        <input type="password" name="password" onChange={e => setPassword(e.target.value)} required />
+                                        <div className="underline" />
+                                        <label>Password</label>
+                                    </div>
+                                    <div className="input-data">
+                                        <input type="password" name="cfpassword" onChange={e => setCfpassword(e.target.value)} required />
+                                        <div className="underline" />
+                                        <label>Confirm Password</label>
+                                    </div>
+                                    <input value={code} name="code" disabled hidden/>
+                                    <div className="error-txt">{errorText}</div>
+                                    <div><button className="btn-signin" type="submit">Signup</button></div>
+                                    <div className="link">Already a member? <a href="#">Signin here</a></div>
+                                </form>
                             </div>
-                            <form className="wrapper">
-                                <div className="input-data">
-                                    <input type="text" required />
-                                    <div className="underline" />
-                                    <label>Fullname</label>
-                                </div>
-                                <div className="input-data">
-                                    <input type="text" required />
-                                    <div className="underline" />
-                                    <label>Email address</label>
-                                </div>
-                                <div className="input-data">
-                                    <input type="text" required />
-                                    <div className="underline" />
-                                    <label>Password</label>
-                                </div>
-                                <div className="input-data">
-                                    <input type="text" required />
-                                    <div className="underline" />
-                                    <label>Confirm Password</label>
-                                </div>
-                                {/* <div className="error-txt">Error text</div> */}
-                                <div><button className="btn-signin" type="submit">Signup</button></div>
-                                <div className="link">Already a member? <a href="#">Signin here</a></div>
-                            </form>
-                        </div>
                         </div>
                     </div>
                 </section>
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 import CustomCard from '../CustomCard';
 
 export const Products = ({products}) => {
-    const [searchTerm, setSearchTerm] = useState();
-    // console.log(Object.keys(products));
+    const [searchTerm, setSearchTerm] = useState('');
 
     return (
         <div className="container">
@@ -12,14 +11,21 @@ export const Products = ({products}) => {
                 <div className="tab__list">
                     <div className="title__container tabs">
                         <div className="search-field">
-                            <input type="text" placeholder="Search here..." onChange={(e) => setSearchTerm(e.target.value)}/>
+                            <input type="text" value={searchTerm} placeholder="Search here..." onChange={e => setSearchTerm(e.target.value)}/>
                         </div>
                     </div>
                 </div>
                 <div className="category__container">
                     <div className="category__center">
                         {
-                            products.map( (product, index) => <CustomCard key={index}  product={product} /> )
+                            products.filter(product => {
+                                if(searchTerm == '') {
+                                    return product;
+                                }
+                                else if(product.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                    return product;
+                                }
+                            }).map( (product, index) => <CustomCard key={index}  product={product} /> )
                         }
                     </div>
                 </div>
